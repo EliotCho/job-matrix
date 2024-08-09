293f0643059dbf6eadea61caa262a749fe90a0f2
Jose <Josey@duck.com>
2024-08-09 11:45:24 -0400

    Got the API to work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
+require('dotenv').config();
 
 // Import all of the routes from /api/ here
 const userRoutes = require("./userRoutes");
 const exampleDataRoutes = require("./exampleDataRoutes");
+const apiId = process.env.API_ID;
+const apiKey = process.env.API_KEY;
+const baseURL = `https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${apiId}&app_key=${apiKey}`;
+
+console.log(baseURL)
 
 // Connect the routes to the router here
 router.use("/users", userRoutes);
 router.use("/exampleData", exampleDataRoutes);
 
 module.exports = router;
+
+
